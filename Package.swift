adeca42bb2254ae312cd75a4a537ffedab36a50f
Denis Chaschin <denis@chaschin.pro>
2021-05-24 23:23:49 +1000

    supported platforms added

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "WrappingStack",
+    platforms: [
+        .iOS(.v9),
+        .watchOS(.v6),
+        .tvOS(.v13),
+        .macOS(.v10_10)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
