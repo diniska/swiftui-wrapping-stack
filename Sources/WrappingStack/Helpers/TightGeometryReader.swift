bd054af91488a8d55d003eeb4f8ddb61fbee8076
Denis Chaschin <denis@chaschin.pro>
2021-05-28 23:22:11 +1000

    TightWidthGeometryReader added

--- Sources/WrappingStack/Helpers/TightGeometryReader.swift
+++ Sources/WrappingStack/Helpers/TightGeometryReader.swift
@@ -2,7 +2,30 @@
 
 import SwiftUI
 
-@available(iOS 14, macOS 11, *)
+@available(iOS 13, macOS 10.15, *)
+struct TightWidthGeometryReader<Content: View>: View {
+    @State private var width: CGFloat = 0
+
+    var content: (GeometryProxy) -> Content
+    
+    init(@ViewBuilder content: @escaping (GeometryProxy) -> Content) {
+        self.content = content
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            content(geometry)
+                .onSizeChange { size in
+                    if self.width != size.width {
+                        self.width = size.width
+                    }
+                }
+        }
+        .frame(width: width)
+    }
+}
+
+@available(iOS 13, macOS 10.15, *)
 struct TightHeightGeometryReader<Content: View>: View {
     @State private var height: CGFloat = 0
 
