f84405c46609108bcf1350a59e480adac697b21e
Denis Chaschin <denis@chaschin.pro>
2021-07-25 21:34:08 +1000

    GeometryReader alignment issue fixed

--- Sources/WrappingStack/Helpers/TightHeightGeometryReader.swift
+++ Sources/WrappingStack/Helpers/TightHeightGeometryReader.swift
@@ -4,11 +4,16 @@ import SwiftUI
 
 @available(iOS 14, macOS 11, *)
 struct TightHeightGeometryReader<Content: View>: View {
+    var alignment: Alignment
     @State private var height: CGFloat = 0
 
     var content: (GeometryProxy) -> Content
     
-    init(@ViewBuilder content: @escaping (GeometryProxy) -> Content) {
+    init(
+        alignment: Alignment = .topLeading,
+        @ViewBuilder content: @escaping (GeometryProxy) -> Content
+    ) {
+        self.alignment = alignment
         self.content = content
     }
     
@@ -20,6 +25,7 @@ struct TightHeightGeometryReader<Content: View>: View {
                         self.height = size.height
                     }
                 }
+                .frame(maxWidth: .infinity, alignment: alignment)
         }
         .frame(height: height)
     }
